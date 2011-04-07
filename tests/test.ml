c9089235a3ae043fddfedee44a569d8829a31b8a
Roman Sokolov <sokolov.r.v@gmail.com>
2011-04-08 11:21:39 +1200

    simple quickcheck test

--- tests/test.ml
+++ tests/test.ml
@@ -2,25 +2,40 @@
 open QuickCheck
 open QuickCheck_gen
 
+open Bson
 
-module Arbitrary_objectid = struct
-  type t = Bson.objectid
-  let arbitrary =
-    such_that (fun s -> String.length s = 12)  Arbitrary_string.arbitrary
-end
+let arbitrary_element = oneof [ret_gen Minkey; ret_gen Maxkey]
 
-module Testable_objectid_to_bool =
-  Testable_fun
-    (Arbitrary_objectid)
-    (PShow_string)
-    (Testable_bool)
+let arbitrary_cstring = arbitrary_string
 
-module CO = Check(Testable_objectid_to_bool)
+let arbitrary_item = arbitrary_pair arbitrary_cstring arbitrary_element
 
-let prop_length oid = String.length oid = 12
+let arbitrary_document = arbitrary_list arbitrary_item
 
+let show_element = function
+  | Minkey -> "Minkey"
+  | Maxkey -> "Maxkey"
+  | _ -> ""
+
+let show_cstring = show_string
+
+let show_item = show_pair show_cstring show_element
+
+let show_document = show_list show_item
+
+let testable_doc_to_bool =
+  testable_fun arbitrary_document show_document testable_bool
+
+let cl = quickCheck testable_doc_to_bool
+
+let prop_parseunparse doc =
+  try
+    decode (encode doc) = doc
+  with MalformedBSON s ->
+    let () = print_endline s in
+    false
 
 let main () =
-  CO.quickCheck prop_length
+  cl prop_parseunparse
 
 let () = main ()
